9c496b9790c432bf9ab160cc9d8bcdb9db7fe0a2
Dejan Appenzeller <dejan.appenzeller@swisspeers.ch>
2021-04-13 16:52:21 +0200

    created DbContext

---
+++ Test/TestContext.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Test
+{
+    public class TestContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; } = null!;
+    }
+
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+    }
+}
